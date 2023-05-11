215429df1640920db80c6c15d5d803250166d665
The-Flying-Dev <leandranm@outlook.com>
2023-05-11 03:13:36 +0200

    temperature

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
 #write your code here
+
+def ftoc(fahrenheit)
+     celsius = (fahrenheit - 32) * 5 / 9
+     celsius
+end 
+
+def ctof(celsius)
+     fahrenheit = ((9.0 / 5.0) * celsius) + 32
+     fahrenheit
+end 
